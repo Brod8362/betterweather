ccf0f762007a434014c0fec51a9fe67764ccfd0e
Brod8362 <brod8362@gmail.com>
2020-12-13 12:05:59 -0600

    Add plugin.yml, config.yml, and enable/disable of weather control

--- src/main/scala/pw/byakuren/weather_plugin/WeatherListener.scala
+++ src/main/scala/pw/byakuren/weather_plugin/WeatherListener.scala
@@ -1,8 +1,16 @@
 package pw.byakuren.weather_plugin
 
-import org.bukkit.event.Listener
+import org.bukkit.Bukkit
+import org.bukkit.event.weather.WeatherChangeEvent
+import org.bukkit.event.{EventHandler, Listener}
 
 class WeatherListener extends Listener {
+
+  @EventHandler
+  def onWeatherChange(event: WeatherChangeEvent) {
+    Bukkit.broadcastMessage("helo, the weather has changed!")
+  }
+
 
 
 }
--- src/main/scala/pw/byakuren/weather_plugin/WeatherPlugin.scala
+++ src/main/scala/pw/byakuren/weather_plugin/WeatherPlugin.scala
@@ -1,14 +1,28 @@
 package pw.byakuren.weather_plugin
 
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.{Bukkit, GameRule}
 import org.bukkit.plugin.java.JavaPlugin
+import scala.collection.JavaConverters._
 
 class WeatherPlugin extends JavaPlugin {
-  override def onEnable(): Unit = {
 
+  val config: FileConfiguration = getConfig
+
+
+  override def onEnable(): Unit = {
+    Bukkit.broadcastMessage("BetterWeather loaded")
+    getServer.getPluginManager.registerEvents(new WeatherListener, this)
+    weatherPluginControl(true)
   }
 
   override def onDisable(): Unit = {
+    weatherPluginControl(false)
+  }
 
+  def weatherPluginControl(enabled: Boolean): Unit = {
+    config.getStringList("worlds").asScala.map(getServer.getWorld(_))
+      .filter(_ != null).foreach(_.setGameRule(GameRule.DO_WEATHER_CYCLE, enabled))
   }
 
 }
