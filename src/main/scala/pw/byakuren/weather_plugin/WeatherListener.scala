9432fe23a670e18a25e56b98baffa35926944651
Brod8362 <brod8362@gmail.com>
2020-12-13 11:25:45 -0600

    Initial commit

---
+++ src/main/scala/pw/byakuren/weather_plugin/WeatherListener.scala
@@ -0,0 +1,8 @@
+package pw.byakuren.weather_plugin
+
+import org.bukkit.event.Listener
+
+class WeatherListener extends Listener {
+
+
+}
---
+++ src/main/scala/pw/byakuren/weather_plugin/WeatherPlugin.scala
@@ -0,0 +1,14 @@
+package pw.byakuren.weather_plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WeatherPlugin extends JavaPlugin {
+  override def onEnable(): Unit = {
+
+  }
+
+  override def onDisable(): Unit = {
+
+  }
+
+}
